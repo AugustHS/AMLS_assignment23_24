9b094ceadb72086e99281a521681260aab21661c
AugustHS <94126893+AugustHS@users.noreply.github.com>
2024-01-25 18:46:00 +0000

    Update main.py

--- AMLS_23_24_SN23077836/main.py
+++ AMLS_23_24_SN23077836/main.py
@@ -1,1 +1,3 @@
 #copy the code from folder 'A' and 'B' according to the instructions in README to run implementation here
+#file 'code for task A' contains the final code for task A
+#file 'code for task B' contains the final code for task B
