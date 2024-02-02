d87e4b0d174810cb2416e41ba351115cfb49d659
AugustHS <94126893+AugustHS@users.noreply.github.com>
2024-02-02 17:42:37 +0000

    Update main.py
    Add some instruction in main.py for the load of datasets

--- AMLS_23_24_SN23077836/main.py
+++ AMLS_23_24_SN23077836/main.py
@@ -1,3 +1,4 @@
 #copy the code from folder 'A' and 'B' according to the instructions in README to run implementation here
 #file 'code for task A' contains the final code for task A
 #file 'code for task B' contains the final code for task B
+#important: if you copy the code from 'A' and 'B' folders to here, please remember to change the '../' in 'data = np.load('../Datasets/<file name>')' into './' or just remove'../',beacuse the relative path of dataset has changed.
